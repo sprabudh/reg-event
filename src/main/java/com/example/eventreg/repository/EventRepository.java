5d8489c48f8c5dd331a7a1ef335089e2020c4396
sprabudh <sevanth.ch@npstx.com>
2026-08-18 23:17:19 +0530

    updated EventRepository

--- src/main/java/com/example/eventreg/repository/EventRepository.java
+++ src/main/java/com/example/eventreg/repository/EventRepository.java
@@ -1,6 +1,8 @@
 package com.example.eventreg.repository;
 
 import com.example.eventreg.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
