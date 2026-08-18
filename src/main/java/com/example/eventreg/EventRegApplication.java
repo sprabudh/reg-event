32af5bb4c88a6b48bd2af2b68430f2a3abf7776f
sprabudh <sevanth.ch@npstx.com>
2026-08-18 17:42:02 +0530

    Initial commit springboot setup with postgree sql

---
+++ src/main/java/com/example/eventreg/EventRegApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventreg/EventRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
